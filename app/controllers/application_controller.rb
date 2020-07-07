cd6d5e2d14972c6cc4d1ede2a7fe9cce5d713e9f
irlgabriel <radu.gabriel131@gmail.com>
2020-07-07 21:10:03 +0300

    DONE!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  private
+
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
+
 
 end
